bc438644c8acd406eaa6121f634475af2cf988e2
Andrea Heßler <andrea.hessler@edu.fh-joanneum.at>
2021-03-14 16:19:01 +0100

    resolved current merge conflict

--- src/main/java/at/fhj/iit/Main.java
+++ src/main/java/at/fhj/iit/Main.java
@@ -10,5 +10,26 @@ public class Main {
 
         Drink d = new SimpleDrink("Rotwein",l);
         System.out.println(d);
+
+        Liquid lt = new Liquid("Rum", 0.125, 43);
+        System.out.println(lt.getName());
+        System.out.println(lt.getVolume());
+
+        Drink t = new Tea("Black Tea with Rum",lt);
+        System.out.println(t);
+
+        //-------------Cocktail Implementation----------------------------------------------------------
+        Liquid dryGin = new Liquid("Dry Gin",0.060, 40);
+        Liquid vermouth = new Liquid("Vermouth",0.010,16);
+
+        Cocktail martini = new Cocktail("Martini");
+        martini.addLiquid(dryGin);
+        martini.addLiquid(vermouth);
+
+        System.out.println("Contents of Martini:"+ Arrays.toString(martini.getLiquids()));
+        System.out.printf("%s is Alcoholic: %b%n",martini.getName(),martini.isAlcoholic());
+        System.out.printf("Alcohol percentage of %s: %.2f%n",martini.getName(),martini.getAlcoholPercent());
+        System.out.printf("Volume of %s: %.2f%n",martini.getName(),martini.getVolume());
+//------------------------------------------------------------------------------------------------
     }
 }
--- src/main/java/at/fhj/iit/Tea.java
+++ src/main/java/at/fhj/iit/Tea.java
@@ -1,2 +1,62 @@
-package at.fhj.iit;public class Tea {
+/**
+ * Tea is a special SimpleDrink
+ * Tea with Alcohol is possible (exp. Tea with Rum)
+ * Author:      Andrea Hessler
+ * Last Change: 14.03.2021
+ */
+
+package at.fhj.iit;
+public class Tea extends Drink{
+    /**
+     * uses only one liquid
+     */
+    protected Liquid l;
+
+    /**
+     * Creates a Tea object with given name and liquid
+     *
+     * @param name name of Tea
+     * @param l only one liquid in Tea, because it's a simple drink an not e.g. a cocktail
+     */
+    Tea(String name, Liquid l){
+        super(name);
+        this.l = l;
+    }
+
+    /**
+     * Returns volume of liquid l
+     *
+     * @return the volume of Tea in litre
+     */
+    @Override
+    public double getVolume() {
+        return l.getVolume();
+    }
+
+    /**
+     * Returns alcohol volume percent of liquid l
+     *
+     * @return alcohol volume percent
+     */
+    @Override
+    public double getAlcoholPercent() {
+        return l.getAlcoholPercent();
+    }
+
+    /**
+     * Gives information if drink is alcoholic or not
+     *
+     * @return true when alcoholic liquids are present, otherwise false
+     */
+    @Override
+    public boolean isAlcoholic() {
+        if(l.getAlcoholPercent() > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "Tea called " + name + " with " + l.getAlcoholPercent() + " percent alcohol by volume";
+    }
 }
