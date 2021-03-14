546939d8a5d4a80b5210841dcfaed7357c9a6be9
Lukas MacBook <lukas.becker.lb@gmail.com>
2021-03-14 15:32:00 +0100

    Added methods to Cocktails and implemented Cocktails in main

--- src/main/java/at/fhj/iit/Main.java
+++ src/main/java/at/fhj/iit/Main.java
@@ -1,5 +1,7 @@
 package at.fhj.iit;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args){
@@ -9,6 +11,20 @@ public class Main {
         System.out.println(l.getVolume());
 
         Drink d = new SimpleDrink("Rotwein",l);
+//-------------Cocktail Implementation----------------------------------------------------------
+        Liquid dryGin = new Liquid("Dry Gin",0.060, 40);
+        Liquid vermouth = new Liquid("Vermouth",0.010,16);
+
+        Cocktail martini = new Cocktail("Martini");
+        martini.addLiquid(dryGin);
+        martini.addLiquid(vermouth);
+
+        System.out.println("Contents of Martini:"+ Arrays.toString(martini.getLiquids()));
+        System.out.printf("%s is Alcoholic: %b%n",martini.getName(),martini.isAlcoholic());
+        System.out.printf("Alcohol percentage of %s: %.2f%n",martini.getName(),martini.getAlcoholPercent());
+        System.out.printf("Volume of %s: %.2f%n",martini.getName(),martini.getVolume());
+//------------------------------------------------------------------------------------------------
+
         System.out.println(d);
     }
 }
