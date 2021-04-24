51de13f1ba43ea6493eb0a8c1157bc730673a61f
lukasbeckercode <lukas.becker.lb@outlook.com>
2021-04-24 16:57:23 +0200

    removed git stash things, cleanup

--- src/test/java/at/fhj/iit/CocktailTest.java
+++ src/test/java/at/fhj/iit/CocktailTest.java
@@ -1,33 +1,21 @@
 package at.fhj.iit;
 
 import org.junit.jupiter.api.BeforeEach;
-<<<<<<< Updated upstream
-import org.junit.jupiter.api.DisplayName;
-=======
->>>>>>> Stashed changes
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CocktailTest {
-<<<<<<< Updated upstream
 
     private Cocktail cocktail;
+    Liquid vodka;
+    Liquid cola;
+    Liquid whiskey;
 
     @BeforeEach
     void setup(){
         cocktail = new Cocktail("test");
     }
-    @DisplayName("getName Test")
-    @Test
-    void getName() {
-        assertEquals("test",cocktail.getName());
-=======
-    Cocktail cocktail;
-    Liquid vodka;
-    Liquid cola;
-    Liquid whiskey;
-
     @BeforeEach
     void beforeEach() {
         cocktail = new Cocktail("test");
@@ -45,38 +33,29 @@ class CocktailTest {
     @Test
     void getName() {
         assertEquals("test", cocktail.getName());
->>>>>>> Stashed changes
     }
 
     @Test
     void getVolume() {
-<<<<<<< Updated upstream
-=======
         cocktail.addLiquid(cola);
         cocktail.addLiquid(vodka);
         assertEquals(0.28,cocktail.getVolume());
->>>>>>> Stashed changes
     }
 
     @Test
     void getAlcoholPercent() {
-<<<<<<< Updated upstream
-=======
         cocktail.addLiquid(vodka);
         cocktail.addLiquid(whiskey);
         assertEquals(32.5, cocktail.getAlcoholPercent());
->>>>>>> Stashed changes
     }
 
     @Test
     void isAlcoholic() {
-<<<<<<< Updated upstream
     }
 
     @Test
     void getLiquids() {
-    }
-=======
+
         cocktail.addLiquid(cola);
         assertFalse(cocktail.isAlcoholic());
         cocktail.addLiquid(whiskey);
@@ -87,5 +66,4 @@ class CocktailTest {
     // void getLiquids() {
     //     fail("Implement me");
     // }
->>>>>>> Stashed changes
 }
