16ae21f1a2015ff137530fb1ea44c73f00763a28
David-Bitcoin <80330268+David-Bitcoin@users.noreply.github.com>
2021-04-24 19:41:23 +0200

    added some comments

--- src/main/java/at/fhj/iit/Coffee.java
+++ src/main/java/at/fhj/iit/Coffee.java
@@ -1,23 +1,49 @@
 package at.fhj.iit;
 
+/**
+ * Coffee consisting of multiple Liquids
+ * @see Drink
+ * @author David Akdogan
+ * last change: 24.04.2021
+ */
+
 public class Coffee extends Drink {
 
     protected Liquid l;
+
+    /**
+     * get the name of the cocktail
+     * @param name name of the drink
+     * get any Liquid
+     */
 
     public Coffee(String name, Liquid l) {
         super(name);
         this.l = l;
     }
 
+    /**
+     * @return the volume of all Liquids
+     */
+
     @Override
     public double getVolume() {
              return l.getVolume();
     }
 
+    /**
+     * @return the AlcoholPercent
+     */
+
     @Override
     public double getAlcoholPercent() {
         return l.getAlcoholPercent();
     }
+
+    /**
+     * checks if any Liquid in the cocktail is alcoholic
+     * @return true if it contains alcohol
+     */
 
     @Override
     public boolean isAlcoholic() {
--- src/test/java/at/fhj/iit/CoffeeTest.java
+++ src/test/java/at/fhj/iit/CoffeeTest.java
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Testcase for the Coffee Class
+ * @see Cocktail
+ * @author David Akdogan
+ * last change: 24.4.2021
+ */
+
 class CoffeeTest {
     private Coffee coffee;
 
@@ -13,16 +20,27 @@ class CoffeeTest {
         coffee = new Coffee("IrishCoffee", new Liquid("whiskey", 0.03, 40));
     }
 
+    /**
+     * tests the Volume
+     */
 
     @Test
     void getVolume() {
         assertEquals(0.03, coffee.getVolume());
     }
 
+    /**
+     * tests the AlcoholPercent
+     */
+
     @Test
     void getAlcoholPercent() {
         assertEquals(40, coffee.getAlcoholPercent());
     }
+
+    /**
+     * tests the isAlcoholic
+     */
 
     @Test
     void isAlcoholic() {
