0f326dbaa0f3502430eb4cfa6f0a79d734e1651c
David-Bitcoin <80330268+David-Bitcoin@users.noreply.github.com>
2021-04-24 19:02:25 +0200

    added coffeeTest

--- src/test/java/at/fhj/iit/CoffeeTest.java
+++ src/test/java/at/fhj/iit/CoffeeTest.java
@@ -1,20 +1,31 @@
 package at.fhj.iit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoffeeTest {
+    private Coffee coffee;
+
+    @BeforeEach
+    void beforeEach(){
+        coffee = new Coffee("IrishCoffee", new Liquid("whiskey", 0.03, 40));
+    }
+
 
     @Test
     void getVolume() {
+        assertEquals(0.03, coffee.getVolume());
     }
 
     @Test
     void getAlcoholPercent() {
+        assertEquals(40, coffee.getAlcoholPercent());
     }
 
     @Test
     void isAlcoholic() {
+        assertTrue(coffee.isAlcoholic());
     }
 }
